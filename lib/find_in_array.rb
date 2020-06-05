5b0e1bde9e6b96a5faa5cf253f4623ea9feb4955
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-05 11:36:28 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,11 +1,11 @@
 def find_element_index(array, value_to_find)
- counter = 0 
- while counter <= array.length 
- do
-   if array[counter]== value_to_find
-     value_to_find = array[counter]
-end
-counter +=1
-end
-nil
+  count = 0
+  found_value_index = nil
+  while count < array.length do
+    if array[count] == value_to_find
+      found_value_index = count
+    end
+    count += 1
+  end
+  found_value_index
 end
